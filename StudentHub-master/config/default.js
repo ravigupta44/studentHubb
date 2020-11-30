fec6d27775154c9d9be4210d02ac2d97572bf99a
Ravi Kumar Gupta <61945913+ravigupta44@users.noreply.github.com>
2020-11-30 23:13:38 +0530

    Add files via upload

---
+++ StudentHub-master/config/default.js
@@ -0,0 +1,7 @@
+//MongoAtlas access URI
+
+module.exports = {
+  mongoURI:
+    "mongodb+srv://kohliharsh:W@terl0705@cluster0.nzdyb.mongodb.net/test?retryWrites=true&w=majority",
+  jwtSecret: "secret",
+};
